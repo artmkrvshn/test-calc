e3af01a8fb255af277923652993760427be60e40
Artem Kryvoshein <artm.krvshn@gmail.com>
2024-12-20 04:46:02 +0100

    Add Main file

--- src/main/java/com/testcalc/Main.java
+++ src/main/java/com/testcalc/Main.java
@@ -2,6 +2,24 @@ package com.testcalc;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        EngineeringCalculator calculator = new EngineeringCalculator();
+
+        System.out.println("Addition: " + calculator.add(5, 3));
+        System.out.println("Subtraction: " + calculator.subtract(5, 3));
+        System.out.println("Multiplication: " + calculator.multiply(5, 3));
+        try {
+            System.out.println("Division: " + calculator.divide(5, 3));
+            System.out.println("Division by zero: " + calculator.divide(5, 0));
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("Power: " + calculator.power(2, 3));
+        try {
+            System.out.println("Square Root: " + calculator.squareRoot(9));
+            System.out.println("Square Root of negative number: " + calculator.squareRoot(-1));
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
